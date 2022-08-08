9275064ef4570a5888eb25997314f655b13efb56
SJPark <pcpark98@naver,com>
2022-08-09 00:41:49 +0900

    chore: 공용 컴포넌트 수정 작업

--- src/duckku-ui/Button/index.jsx
+++ src/duckku-ui/Button/index.jsx
@@ -1,14 +1,16 @@
-import styled, { css, withTheme } from "styled-components";
+import styled, { withTheme } from "styled-components";
 
 const Button = styled.button`
   position: relative;
-  height: ${(props) => (props.height ? props.height : "64")}px;
-  width: ${(props) => (props.width ? props.width : "384")}px;
+  height: ${(props) => (props.height ? props.height : "65")}px;
+  width: ${(props) => (props.width ? props.width : "346")}px;
   border: none;
   border-radius: ${(props) =>
     props.borderRadius ? props.borderRadius : "16"}px;
   font-size: ${(props) => (props.fontSize ? props.fontSize : "24")}px;
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "700")};
+  font-family: ${(props) =>
+    props.fontFamily ? props.fontFamily : "Pretendard-Bold"};
   text-align: center;
   color: ${(props) => (props.color ? props.color : props.theme.colors.white)};
   background: ${(props) =>
@@ -18,27 +20,6 @@ const Button = styled.button`
   z-index: inherit;
   line-height: 29px;
   letter-spacing: 0.55px;
-
-  ${(props) =>
-    props.large &&
-    css`
-      width: 80px;
-      height: 350px;
-    `};
-
-  ${(props) =>
-    props.medium &&
-    css`
-      width: 50px;
-      height: 250px;
-    `};
-
-  ${(props) =>
-    props.small &&
-    css`
-      width: 35px;
-      height: 200px;
-    `};
 `;
 
 export default withTheme(Button);
