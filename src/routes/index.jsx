6222c8d030bf2fb889ee38619ef178ec3f4dc5b6
pakxe <pigkill40@naver.com>
2022-08-09 18:35:01 +0900

    feat: NotFound 페이지 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Intro from "../pages/Intro";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
-
+import NotFound from "../pages/NotFound";
 // 라우트명은 kebab-case 로 작성합니다
 
 const Router = () => (
@@ -11,6 +11,7 @@ const Router = () => (
       <Route exact path="/" element={<Intro />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/sign-up" element={<SignUp />} />
+      <Route exact path="/not-found" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
