ea936950d53e821dede7ffc3605ecdd33f0a1291
miri <oo117mmm@gmail.com>
2022-08-10 19:16:41 +0900

    chore: 소장중 아이콘 추가

--- src/duckku-ui/StoreInfo/index.jsx
+++ src/duckku-ui/StoreInfo/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import loveImg from "../../assets/images/love.jpeg";
+import { HiOutlineCheckCircle } from "react-icons/hi";
 
 const Container = styled.div`
   width: 100vw;
@@ -7,7 +8,6 @@ const Container = styled.div`
   flex-direction: row;
   overflow: scroll;
   padding-left: 32px;
-  margin-left: -px;
 `;
 
 const Box = styled.div`
@@ -16,6 +16,7 @@ const Box = styled.div`
   align-items: flex-start;
   flex-direction: column;
   padding-right: 18px;
+  position: relative;
 `;
 
 const ImageContainer = styled.div`
@@ -52,11 +53,51 @@ const Detail = styled.div`
   color: #444444;
 `;
 
+const IconBox = styled.div`
+  width: 154px;
+  height: 32px;
+  padding-top: 8px;
+  padding-right: 8px;
+  position: absolute;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+`;
+
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: #ffffff;
+  border-radius: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  opacity: 90%;
+  object-fit: cover;
+`;
+
 const moveToPage = () => {};
 const StoreInfo = () => {
   return (
     <Container>
+      <svg width="0" height="0">
+        <linearGradient id="purple" x1="100%" y1="100%" x2="0%" y2="0%">
+          <stop stopColor="#7001FF" offset="0" />
+          <stop stopColor="#C15CFF" offset="50" />
+        </linearGradient>
+      </svg>
+
       <Box>
+        <IconBox>
+          <Circle>
+            <HiOutlineCheckCircle
+              size="32px"
+              opacity="100%"
+              style={{ stroke: "url(#purple)" }}
+            />
+          </Circle>
+        </IconBox>
+
         <ImageContainer>
           <ImageBox src={loveImg} onClick={moveToPage} />
         </ImageContainer>
