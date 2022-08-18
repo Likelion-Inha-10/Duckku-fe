8c1a8498d3ce678e597cbdc091c025bbc47de7f6
sean2337 <sean2337@naver.com>
2022-08-18 23:41:38 +0900

    chore:찜한 앨범 map함수 수정

--- src/pages/Wish/index.jsx
+++ src/pages/Wish/index.jsx
@@ -10,39 +10,61 @@ const AllWishWrapper = styled.div`
   height: auto;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: start;
+  padding-left: 25px;
 `;
 
+const AlbumListsMap = (props) => {
+  return props.Lists.map((v) => (
+    <WishCard
+      key={v.Singer}
+      Img={v.Img}
+      SingTitle={v.SingTitle}
+      Singer={v.Singer}
+      SingYear={v.SingYear}
+    />
+  ));
+};
+
 const Wish = () => {
+  const Lists = [
+    {
+      Img: "https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg",
+      SingTitle: "More and More",
+      Singer: "트와이스",
+      SingYear: "2022",
+    },
+    {
+      Img: "https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg",
+      SingTitle: "More and More",
+      Singer: "트와이스",
+      SingYear: "2022",
+    },
+    {
+      Img: "https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg",
+      SingTitle: "More and More",
+      Singer: "트와이스",
+      SingYear: "2022",
+    },
+    {
+      Img: "https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg",
+      SingTitle: "More and More",
+      Singer: "트와이스",
+      SingYear: "2022",
+    },
+    {
+      Img: "https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg",
+      SingTitle: "More and More",
+      Singer: "트와이스",
+      SingYear: "2022",
+    },
+  ];
   return (
     <Layout>
       <AllWishWrapper>
         <Header back title=" 찜한 앨범" />
         <Margin height="80" />
-        <WishCard
-          Img="https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg"
-          SingTitle="More and More"
-          Singer="트와이스"
-          SingYear="2022"
-        />
-        <WishCard
-          Img="https://upload.wikimedia.org/wikipedia/en/2/20/Iz%2AOne_-_Twelve_A.jpg"
-          SingTitle="More and More"
-          Singer="트와이스"
-          SingYear="2022"
-        />
-        <WishCard
-          Img="https://images.genius.com/bfdf3da954dc9937c0e2bac2ea124576.1000x1000x1.png"
-          SingTitle="More and More"
-          Singer="트와이스"
-          SingYear="2022"
-        />
-        <WishCard
-          Img="https://blogger.googleusercontent.com/img/a/AVvXsEgc5SNyrAlpMMg4ovzxpIeYf6bk8uqdWhGLYOeKIpj-lhHXxmOXDrqFCc-RrkpIyGpbqovo3aTZ5jbb_PMOuGzQLcEpO7Nkzn61oSl9xqJLB8bmnrvieq9tD5qO4_LMpecp3r1d_ICjf62eul-cryA9IVYRo-dSN_5TniWYCYePgaDdedOmwrqV62Ix=w640-h568"
-          SingTitle="More and More"
-          Singer="트와이스"
-          SingYear="2022"
-        />
+        <AlbumListsMap Lists={Lists} />
       </AllWishWrapper>
     </Layout>
   );
